afd182fe7795b22650daa60036334e1a73dd6e4e
bertrand <bertrand@leserveur.casper>
2026-03-29 14:43:35 +0200

    Translate isWeekend for /bin/sh

--- arr-stack/isWeekend.sh
+++ arr-stack/isWeekend.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-DOW=$(printf '%(%u)T')  # equals to 2 on Tuesdays
+DOW=$(date +%u)   # 1=Monday ... 7=Sunday
 HOUR=$(date +%H)
 
-if [ $DOW -eq 6 ] && [ $HOUR -gt 20 ]
-then
-	exit 0
-elif [ $DOW -eq 7 ] && [ $HOUR -lt 16 ]
-then
-	exit 0
+if [ "$DOW" -eq 6 ] && [ "$HOUR" -gt 20 ]; then
+    exit 0
+elif [ "$DOW" -eq 7 ] && [ "$HOUR" -lt 16 ]; then
+    exit 0
 else
-	exit 1
+    exit 1
 fi
